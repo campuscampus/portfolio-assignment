9f04546ef2d11ecc71173d4c305246ccae12cc70
Vaibhav <1mission2027@gmail.com>
2026-06-27 18:46:47 +0530

    a

--- js1.js
+++ js1.js
@@ -4,3 +4,9 @@ const navbar = document.querySelector(".navbar");
 toggle.addEventListener("click", () => {
     navbar.classList.toggle("nav-open");
 });
+
+const themeButton = document.querySelector("#theme-toggle");
+
+themeButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
